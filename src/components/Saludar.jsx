1c972d157b4602361be45228cfdd58eb4756bd9b
Leonardo Recalde <leo03recalde@gmail.com>
2024-09-10 11:07:52 -0300

    punto2

---
+++ src/components/Saludar.jsx
@@ -0,0 +1,11 @@
+
+
+const Saludar = ({hola}) => {
+    return (
+        <div>
+            <h1 className='text-center pt-5 display-1'>Hello {hola}</h1>
+        </div>
+    );
+};
+
+export default Saludar;
